41928c980daaa00529dcf63d5db14aa6a34d3ab9
dziermanfelix <dziermanfelix@gmail.com>
2025-02-06 21:26:54 -0600

    fix run server command, fix dropDb script

--- tools/dropDb.js
+++ tools/dropDb.js
@@ -1,7 +1,5 @@
 import Jukebox from '../models/JukeboxModel.js';
-import Queue from '../models/QueueModel.js';
 import Session from '../models/SessionModel.js';
-import AccessToken from '../models/AccessTokenModel.js';
 import mongoose from 'mongoose';
 import { mongoUrl } from '../utils/environmentVariables.js';
 
@@ -22,7 +20,5 @@ const drop = async (table, name) => {
 };
 
 await drop(Jukebox, 'jukebox');
-await drop(Queue, 'queue');
-await drop(AccessToken, 'accesstoken');
 await drop(Session, 'session');
 process.exit(0);

687a121ebe10d359e6133bdd7cfdf9ca436c42de
dziermanfelix <dziermanfelix@gmail.com>
2025-02-09 13:17:00 -0600

    adding debug print to find why sockets aren't connecting

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,5 +2,8 @@ import io from 'socket.io-client';
 import { serverUrl, serverPort } from '../utils/frontEndEnv';
 
 export function createSocketConnection(jukeboxName, sessionId) {
+  console.log(
+    `creating socket conn with ${serverUrl}:${serverPort}/?jukeboxName=${jukeboxName}&&sessionId=${sessionId}`
+  );
   return io(`${serverUrl}:${serverPort}/?jukeboxName=${jukeboxName}&&sessionId=${sessionId}`);
 }

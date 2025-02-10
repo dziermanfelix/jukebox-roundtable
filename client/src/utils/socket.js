889ee79e3ca6a736ee302eeae6c33e135ae175d0
dziermanfelix <dziermanfelix@gmail.com>
2025-02-10 07:26:02 -0600

    testing web socket client connection without port

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -2,8 +2,8 @@ import io from 'socket.io-client';
 import { serverUrl, serverPort } from '../utils/frontEndEnv';
 
 export function createSocketConnection(jukeboxName, sessionId) {
-  const connString = `${serverUrl}:${serverPort}/?jukeboxName=${jukeboxName}&&sessionId=${sessionId}`;
-  // const connString = `${serverUrl}/?jukeboxName=${jukeboxName}&&sessionId=${sessionId}`;
+  // const connString = `${serverUrl}:${serverPort}/?jukeboxName=${jukeboxName}&&sessionId=${sessionId}`;
+  const connString = `${serverUrl}/?jukeboxName=${jukeboxName}&&sessionId=${sessionId}`;
   console.log(`creating socket conn with ${connString}`);
   return io(connString, { transports: ['websocket'] });
 }

1be294a7eec77d3e49e859c1ae4e71a01abd8c57
dziermanfelix <dziermanfelix@gmail.com>
2025-02-10 07:37:07 -0600

    adding logic to change the server url for the client web socket based on if the app is local or in production

--- client/src/utils/frontEndEnv.js
+++ client/src/utils/frontEndEnv.js
@@ -1,2 +1,3 @@
 export const serverPort = process.env.SERVER_PORT;
 export const serverUrl = process.env.SERVER_URL;
+export const socketEnv = process.env.SOCKET_ENV;
--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,9 +1,11 @@
 import io from 'socket.io-client';
-import { serverUrl, serverPort } from '../utils/frontEndEnv';
+import { serverUrl, serverPort, socketEnv } from '../utils/frontEndEnv';
 
 export function createSocketConnection(jukeboxName, sessionId) {
-  // const connString = `${serverUrl}:${serverPort}/?jukeboxName=${jukeboxName}&&sessionId=${sessionId}`;
-  const connString = `${serverUrl}/?jukeboxName=${jukeboxName}&&sessionId=${sessionId}`;
+  let connString = `${serverUrl}/?jukeboxName=${jukeboxName}&&sessionId=${sessionId}`;
+  if (socketEnv === 'local') {
+    connString = `${serverUrl}:${serverPort}/?jukeboxName=${jukeboxName}&&sessionId=${sessionId}`;
+  }
   console.log(`creating socket conn with ${connString}`);
   return io(connString, { transports: ['websocket'] });
 }

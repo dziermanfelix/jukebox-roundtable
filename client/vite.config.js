41633103ef4e45111d63b9d522a47c0bf8c16b44
dziermanfelix <dziermanfelix@gmail.com>
2025-07-03 07:25:47 -0500

    integrating tailwind into frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { globalServerPort } from '../common/api';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default ({ mode }) => {
   const env = loadEnv(mode, path.resolve(__dirname, '..'), '');
   return defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     define: {
       'process.env': env,
     },

a04c04a297a6dabbb66929497d9594cd4e7f0bf8
dziermanfelix <dziermanfelix@gmail.com>
2025-07-04 09:43:33 -0500

    cleaning up submit button css

--- client/src/components/SubmitButton.jsx
+++ client/src/components/SubmitButton.jsx
@@ -4,7 +4,7 @@ const SubmitButton = ({ display, submittingDisplay }) => {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   return (
-    <button className='submit-button' type='submit' disabled={isSubmitting}>
+    <button className='submit-btn' type='submit' disabled={isSubmitting}>
       {isSubmitting ? submittingDisplay : display}
     </button>
   );
